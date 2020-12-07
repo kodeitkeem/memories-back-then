c4fb46c16a19d08903f10dd8ba17d6a98d929163
Akeem Dainty <adainty@businessinsider.com>
2020-12-07 01:33:45 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require modules
+const express = require('express');
+
+// Create express app
+
+
+// Connect to MongoDB with mongoose
+
+
+// Require routes
+
+
+// Configure server settings - app.set()
+
+
+// mount middleware - app.use()
+
+// Add session middleware
+
+// Add passport middleware
+
+// Mount routes
+
+// Tell app to listen
